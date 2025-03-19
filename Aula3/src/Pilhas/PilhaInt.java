6de549017695307037b9c13c9ce30fa0423cd763
Guilhermo <guilhermo.payossin@gmail.com>
2025-03-19 20:54:25 -0300

    Add files via upload

---
+++ Aula3/src/Pilhas/PilhaInt.java
@@ -0,0 +1,33 @@
+package Pilhas;
+
+public class PilhaInt {
+    final int N = 6;
+    int[] dados = new int[N];
+    int topo;
+
+    public void init() {
+        topo = 0;
+    }
+
+    public boolean isFull() {
+        return topo == N;
+    }
+
+    public void push(int elem) {
+        if (isFull()) {
+            System.out.println("Stack Overflow");
+            return;
+        }
+        dados[topo] = elem;
+        topo++;
+    }
+
+    public int pop() {
+        if (!isFull()) {
+            System.out.println("Nothing to pop");
+            return 0;
+        }
+        topo--;
+        return dados[topo];
+    }
+}
---
+++ Aula3/src/teste/MainTeste.java
@@ -0,0 +1,25 @@
+package teste;
+
+import Pilhas.PilhaInt;
+
+import java.util.Stack;
+
+public class MainTeste {
+    public static void main(String[] args) {
+        PilhaInt pilha = new PilhaInt();
+        pilha.init();
+        pilha.push(10);
+        pilha.push(12);
+        pilha.push(10);
+        pilha.push(22);
+        pilha.push(33);
+        pilha.push(33);
+        pilha.push(44);
+        System.out.println("Valor retirado da pilha: " + pilha.pop());
+        System.out.println("Valor retirado da pilha: " + pilha.pop());
+        System.out.println("Valor retirado da pilha: " + pilha.pop());
+        System.out.println("Valor retirado da pilha: " + pilha.pop());
+        System.out.println("Valor retirado da pilha: " + pilha.pop());
+        System.out.println("Valor retirado da pilha: " + pilha.pop());
+    }
+}
